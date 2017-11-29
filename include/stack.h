2c1bbd4c9a4de80064ee7dcde8ba5a0d5fcecbc6
alexeyhorkin <alexeykhorkin12@gmail.com>
2017-11-29 23:59:48 +0300

    without gtests

--- include/stack.h
+++ include/stack.h
@@ -1,4 +1,114 @@
+
+#ifndef __STACK_H__
+#define __STACK_H__
+#include <iostream>
+using namespace std;
+
 // объявление и реализация шаблонного стека
+template <class ValType> 
+class Stack {
+private:
+	ValType * arr;
+	int SizeMax;
+	int topmember; //Количество элементов в стеке
+
+public:
+	Stack(int Size_Max=10);
+		~Stack();
+	Stack(const Stack& a);
+	void push(const ValType a); // Положить элемент на вершину стека
+	ValType pop(); // Удалить с вершины стека и вернуть
+	void dell();
+	bool IsEmpty(); // Проверить пустой ли
+	ValType Check(); //  Посмотреть верхний элемент
+	int CurrentSize(); // Количество элементов в стеке
+	void Clear();
+};
+//////////////////////////////////////////////////////////////
+
+template <class ValType>
+Stack <ValType> ::Stack(int Size_Max)
+{
+	SizeMax = Size_Max;
+	topmember = -1;
+	arr = new ValType[SizeMax];
+}
+///////////////////////////////////////////////////////////////
+template <class ValType>
+Stack <ValType> ::~Stack()
+{
+	delete[] arr;
+}
+///////////////////////////////////////////////////////////////
+template <class ValType>
+ Stack <ValType> :: Stack(const Stack& a)
+{
+	SizeMax = a.SizeMax;
+	delete[] arr;
+	arr = new ValType[SizeMax];
+	for (int i = 0; i < topmember+1; i++)
+		arr[i] = a.arr[i];
+}
+ /////////////////////////////////////////////////////////////
+ template <class ValType>
+ void Stack <ValType>::push(const ValType a)
+ {
+	 if (topmember + 1 < SizeMax)
+		 arr[++topmember] = a;
+	 else {
+		 Stack <ValType> Copy(*this);
+		 delete[] arr;
+		 SizeMax= SizeMax*2;
+		 arr = new  ValType[SizeMax];
+		 for (int i = 0; i <= topmember; i++)
+			 arr[i] = Copy.arr[i];
+		 arr[++topmember] = a;
+	 }
+ }
+ ////////////////////////////////////////////////////////////
+ template <class ValType>
+ void Stack <ValType>::dell() {
+
+	 if (topmember != -1)
+		 topmember -= 1;
+	 else throw "lalala";
+ }
+ ////////////////////////////////////////////////////////////
+ template <class ValType>
+ ValType Stack <ValType>::pop()
+ {
+	 if(topmember!=-1)
+	 return arr[topmember--];
+	 else throw "lalala";
+ }
+ ////////////////////////////////////////////////////////////
+ template <class ValType>
+ bool  Stack <ValType>::IsEmpty()
+ {
+	 if (topmember == -1) return 1; else return 0;
+ }
+ ////////////////////////////////////////////////////////////
+ template <class ValType>
+ ValType  Stack <ValType>::Check()
+ {
+	 if(topmember!=-1)
+	 return arr[topmember];
+	 else throw "lalala";
+ }
+ ///////////////////////////////////////////////////////////
+ template <class ValType>
+ int  Stack <ValType>::CurrentSize()
+ {
+	 return topmember + 1;
+ }
+ ////////////////////////////////////////////////////////////
+ template <class ValType>
+ void  Stack <ValType>::Clear()
+ {
+	 topmember = -1;
+ }
+ ///////////////////////////////////////////////////////////
+
 // стек поддерживает операции: 
 // - вставка элемента, 
 // - извлечение элемента, 
@@ -7,3 +117,4 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыделяться память
+#endif;
--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,37 @@
 // реализация пользовательского приложения
+#include <string>
+#include "iostream"
+#include "arithmetic.h"
 
+using namespace std;
 int main()
 {
+	int q = 0;
+	string a; int res; Lexem *LexemPtr;
+	int k = 0;
+	while (k == 0) {
+		cout << "Write your string" << endl;
+		cin >> a;
+		k = ChekString(a);
+	}
+	Arifmetic A(a);
+	A.GetArrLexOne();
+	A.PrintArrLex();
+	A.GetArrLexTwo();
+	A.PrintArrLex2();
+	while (q != 2)
+	{
+		A.Lala();
+		A.Calculete();
+		cout << "If you want to change the variables, press 1, else 2" << endl;
+		cin >> q;
+	}
+
+
   return 0;
 }
+//проверка на корректность строки
+// создание массива лексем 
+// преобразование массива лексем с помощью двух стеков в отсортированный массив лексем без ( ) 
+// Этап вычисления - сбор данных о переменных , вычисление, представить возможность ввода других переменных
+//вывод результата
